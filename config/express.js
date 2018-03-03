6283d9098516627b97209811c21b2d0a3e7c4f82
Pleshko Pavel <pavelpleshko92@mail.ru>
2018-03-03 07:22:15 +0300

    master

--- config/express.js
+++ config/express.js
@@ -1,23 +1,17 @@
 
-var path = require('path');
+
 var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
+
 var session = require('express-session');
 var express = require('express');
 var passport = require('passport');
 var config = require('./index');
-var cookieParser = require('cookie-parser');
-const MongoStore = require('connect-mongo')(session);
+
 module.exports.init = function(app) {
   var env = app.get('env');
   var root = app.get('root');
 
- const sessionOpts = {
-    secret: config.session.secret,
-    key: 'skey.sid',
-    resave: config.session.resave,
-    saveUninitialized: config.session.saveUninitialized
-  };
+ 
 
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
