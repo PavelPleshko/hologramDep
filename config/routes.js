29f23ae5284f377d6e697657ff761618926d6924
Pleshko Pavel <pavelpleshko92@mail.ru>
2018-03-03 07:29:58 +0300

    master

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,7 @@
 module.exports.init = initRoutes;
 
 function initRoutes(app) {
-  let routesPath = app.get('root') + '/app/routes';
+  var routesPath = app.get('root') + '/app/routes';
 
   app.use('/auth', require(routesPath + '/auth'));
  app.use('/api', require(routesPath + '/profile'));
